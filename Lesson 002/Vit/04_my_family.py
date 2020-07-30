9b6fef5edfc1697d3605b4d725febb75c84e47d3
vit <tomas_anderson@mail.ru>
2020-07-30 23:28:03 +0300

    Lesson 002 Task 04 complite

--- Lesson 002/Vit/04_my_family.py
+++ Lesson 002/Vit/04_my_family.py
@@ -5,16 +5,23 @@
 
 # моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
 my_family = []
-
+my_family.append('Dima')
+my_family.append('Olga')
+my_family.append('Alexander')
 
 # список списков приблизителного роста членов вашей семьи
 my_family_height = [
     # ['имя', рост],
-    [],
+    ['Dima', 183],
 ]
+my_family_height.append(['Olga', 174])
+my_family_height.append(['Alexander', 187])
 
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
+print('Рост отца ' + str(my_family_height[2][1]) + ' см')
 
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
+sum_rost = my_family_height[0][1] + my_family_height[1][1] + my_family_height[2][1]
+print('Общий рост моей семьи - ' + str(sum_rost) + ' см')
