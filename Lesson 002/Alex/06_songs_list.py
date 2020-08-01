f7923fbb810e20510f285fad148817e737ecd425
Алексей <AKS2302@yandex.ru>
2020-08-01 09:48:37 +0700

    упражнение_06

--- Lesson 002/Alex/06_songs_list.py
+++ Lesson 002/Alex/06_songs_list.py
@@ -20,7 +20,11 @@ violator_songs_list = [
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
-# TODO здесь ваш код
+time_4_6_9 = violator_songs_list[3][-1] + violator_songs_list[5][-1] + violator_songs_list[8][-1]
+
+print('Три песни звучат', round(time_4_6_9, 2), 'минут')
+
+
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -38,4 +42,7 @@ violator_songs_dict = {
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
-# TODO здесь ваш код
+time_2_7_8 = violator_songs_dict['Personal Jesus'] + violator_songs_dict['Policy of Truth'] + violator_songs_dict['Blue Dress']
+
+print('А другие три песни звучат', time_2_7_8, 'минут')
+
