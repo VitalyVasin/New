261b93ee01dca57a58e2fc2ec57827b2bbe39aea
vit <tomas_anderson@mail.ru>
2020-08-03 12:56:13 +0300

    Lesson 002 Task 08 complite

--- Lesson 002/Vit/08_garden.py
+++ Lesson 002/Vit/08_garden.py
@@ -8,20 +8,24 @@ garden = ('ромашка', 'роза', 'одуванчик', 'ромашка', 
 meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер', 'мак', 'одуванчик', 'ромашка', )
 
 # создайте множество цветов, произрастающих в саду и на лугу
-# garden_set =
-# meadow_set =
-# TODO здесь ваш код
+garden_set = set(garden)
+meadow_set = set(meadow)
+
 
 # выведите на консоль все виды цветов
-# TODO здесь ваш код
+flowers = garden_set | meadow_set
+print(flowers)
 
 # выведите на консоль те, которые растут и там и там
-# TODO здесь ваш код
-
+flowers2 = garden_set & meadow_set
+print(flowers2)
 # выведите на консоль те, которые растут в саду, но не растут на лугу
-# TODO здесь ваш код
+flowers3 = garden_set - meadow_set
+print(flowers3)
 
 # выведите на консоль те, которые растут на лугу, но не растут в саду
-# TODO здесь ваш код
+flowers4 = meadow_set - garden_set
+print(flowers4)
+
 
 
