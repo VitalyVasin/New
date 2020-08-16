38e5f8124675e28361c1b29cbd5f1149ef5562c3
Алексей <AKS2302@yandex.ru>
2020-08-16 17:31:07 +0700

    Lesson 003_01 complite Alex

--- Lesson 003/Alex/01_days_in_month.py
+++ Lesson 003/Alex/01_days_in_month.py
@@ -10,5 +10,30 @@
 user_input = input("Введите, пожалуйста, номер месяца: ")
 month = int(user_input)
 print('Вы ввели', month)
+if month == 1:
+    print('31')
+elif month == 2:
+    print('28')
+elif month == 3:
+    print('31')
+elif month == 4:
+    print('30')
+elif month == 5:
+    print('31')
+elif month == 6:
+    print('30')
+elif month == 7:
+    print('31')
+elif month == 8:
+    print('31')
+elif month == 9:
+    print('30')
+elif month == 10:
+    print('31')
+elif month == 11:
+    print('30')
+elif month == 12:
+    print('31')
+else:
+    print('Номер месяца введен не корректно, необходимо ввести число 1 до 12')
 
-# TODO здесь ваш код
