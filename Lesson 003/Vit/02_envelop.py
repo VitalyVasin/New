ca4b0e50fd944adb0897d3fe00444919dfebbf56
vit <tomas_anderson@mail.ru>
2020-08-28 20:55:53 +0300

    Ready HomeWork Lesson 003-02

--- Lesson 003/Vit/02_envelop.py
+++ Lesson 003/Vit/02_envelop.py
@@ -20,7 +20,14 @@ paper_x, paper_y = 8, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if paper_x < 10:
+    if paper_y < 7:
+        print('ДА')
+    else:
+        print('НЕТ')
+else:
+    print('НЕТ')
+
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
@@ -28,14 +35,14 @@ paper_x, paper_y = 8, 9
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
+hole_x, hole_y = 8, 9
 # brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
 # brick_x, brick_y, brick_z = 2, 10, 11
 # brick_x, brick_y, brick_z = 2, 11, 10
-# brick_x, brick_y, brick_z = 3, 5, 6
+brick_x, brick_y, brick_z = 3, 5, 6
 # brick_x, brick_y, brick_z = 3, 6, 5
 # brick_x, brick_y, brick_z = 6, 3, 5
 # brick_x, brick_y, brick_z = 6, 5, 3
@@ -49,4 +56,20 @@ paper_x, paper_y = 8, 9
 # brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if brick_x < hole_x:
+    if brick_y < hole_y:
+        print('ДА')
+    else:
+        print('НЕТ')
+elif brick_y < hole_x:
+    if brick_z < hole_y:
+        print('ДА')
+    else:
+        print('НЕТ')
+elif brick_x < hole_x:
+    if brick_z < hole_y:
+        print('ДА')
+    else:
+        print('НЕТ')
+else:
+    print('НЕТ')
