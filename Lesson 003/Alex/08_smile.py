5be4052ee2157c3b28914b907c510d383be36947
Alexey <AKS2302@yandex.ru>
2020-11-23 22:29:14 +0700

    Добавил Lesson 004

--- Lesson 003/Alex/08_smile.py
+++ Lesson 003/Alex/08_smile.py
@@ -4,29 +4,32 @@
 import simple_draw as sd
 
 sd.resolution = (600, 600)
-point = sd.random_point()
+x = sd.random_number(0, 600)
+y = sd.random_number(0, 600)
 
 
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 
-def smile(point, color=sd.COLOR_YELLOW):
-        point2 = sd.get_point(280, 315)
-        point3 = sd.get_point(320, 315)
-        point_ellipse1 = sd.get_point(230, 250)
-        point_ellipse2 = sd.get_point(370, 350)
-        line_point1 = sd.get_point(270, 290)
-        line_point2 = sd.get_point(280, 280)
-        line_point3 = sd.get_point(320, 280)
-        line_point4 = sd.get_point(330, 290)
+def smile(x, y):
+        point2 = sd.get_point(x, y)
+        point3 = sd.get_point(x + 40, y)
+        point_ellipse1 = sd.get_point(x - 50, y - 65)
+        point_ellipse2 = sd.get_point(x + 90, y + 35)
+        line_point1 = sd.get_point(x - 10, y - 25)
+        line_point2 = sd.get_point(x, y - 35)
+        line_point3 = sd.get_point(x + 40, y - 35)
+        line_point4 = sd.get_point(x + 50, y - 25)
         radius2 = 5
-        sd.circle(center_position=point2, radius=radius2, width=1)
-        sd.circle(center_position=point3, radius=radius2, width=1)
-        sd.line(start_point=line_point1, end_point=line_point2, width=1)
-        sd.line(start_point=line_point2, end_point=line_point3, width=1)
-        sd.line(start_point=line_point3, end_point=line_point4, width=1)
-        sd.ellipse(left_bottom=point_ellipse1, right_top=point_ellipse2, width=1)
+        sd.circle(center_position=point2, radius=radius2, width=3)
+        sd.circle(center_position=point3, radius=radius2, width=3)
+        sd.line(start_point=line_point1, end_point=line_point2, width=3)
+        sd.line(start_point=line_point2, end_point=line_point3, width=3)
+        sd.line(start_point=line_point3, end_point=line_point4, width=3)
+        sd.ellipse(left_bottom=point_ellipse1, right_top=point_ellipse2, width=3)
 
-smile(point=point)
+for _ in range(10):
+        smile(x=x, y=y)
+
 
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
