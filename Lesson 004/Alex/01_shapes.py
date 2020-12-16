598a92be30124227ab21174c593a7ecc0d7ffd03
Alexey <AKS2302@yandex.ru>
2020-12-16 22:15:02 +0700

    Lesson 004_01 Complete part2

--- Lesson 004/Alex/01_shapes.py
+++ Lesson 004/Alex/01_shapes.py
@@ -33,8 +33,6 @@ import simple_draw as sd
 def triangle(point, angle, length, width):
     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=width)
     v1.draw()
-# for angle in range(0, 241, 120):
-#     triangle(point=point, angle=angle, length=200)
     v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=length, width=width)
     v2.draw()
     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=length, width=width)
@@ -47,8 +45,7 @@ def square (point, angle, length, width):
     v2.draw()
     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 180, length=length, width=width)
     v3.draw()
-    v4 = sd.line(start_point=v3.end_point,end_point=point, width=width)
-    # v4.draw()
+    sd.line(start_point=v3.end_point,end_point=point, width=width)
 
 def pentagon (point, angle, length, width):
     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=width)
@@ -59,8 +56,7 @@ def pentagon (point, angle, length, width):
     v3.draw()
     v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 216, length=length, width=width)
     v4.draw()
-    v5 = sd.line(start_point=v4.end_point, end_point=point, width=width)
-    # v5.draw()
+    sd.line(start_point=v4.end_point, end_point=point, width=width)
 
 def hexagon (point, angle, length, width):
     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=width)
@@ -73,17 +69,77 @@ def hexagon (point, angle, length, width):
     v4.draw()
     v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 240, length=length, width=width)
     v5.draw()
-    v6 = sd.line(start_point=v5.end_point, end_point=point, width=width)
+    sd.line(start_point=v5.end_point, end_point=point, width=width)
 
 point_1 = sd.get_point(100, 100)
 point_2 = sd.get_point(400, 100)
 point_3 = sd.get_point(100, 400)
 point_4 = sd.get_point(400, 400)
+point_5 = sd.get_point(300, 300)
 
-triangle(point=point_1, angle=18, length=150, width=1)
-square(point=point_2, angle=28, length=150, width=1)
-pentagon(point=point_3, angle=24, length=100, width=1)
-hexagon(point=point_4, angle=10, length=100, width=1)
+triangle(point=point_1, angle=18, length=150, width=2)
+square(point=point_2, angle=28, length=150, width=2)
+pentagon(point=point_3, angle=24, length=100, width=2)
+hexagon(point=point_4, angle=10, length=100, width=2)
+
+def figure (point, number_sides, angle, length, width):
+
+    if number_sides == 3:
+        v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=width)
+        v1.draw()
+        v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=length, width=width)
+        v2.draw()
+        v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=length, width=width)
+        v3.draw()
+        print('It is triangle')
+
+    if number_sides == 4:
+        v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=width)
+        v1.draw()
+        v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 90, length=length, width=width)
+        v2.draw()
+        v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 180, length=length, width=width)
+        v3.draw()
+        sd.line(start_point=v3.end_point, end_point=point, width=width)
+        print('It is square')
+
+
+    if number_sides == 5:
+        v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=width)
+        v1.draw()
+        v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 72, length=length, width=width)
+        v2.draw()
+        v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 144, length=length, width=width)
+        v3.draw()
+        v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 216, length=length, width=width)
+        v4.draw()
+        sd.line(start_point=v4.end_point, end_point=point, width=width)
+        print('It is pentagon')
+
+
+    if number_sides == 6:
+        v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=width)
+        v1.draw()
+        v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 60, length=length, width=width)
+        v2.draw()
+        v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 120, length=length, width=width)
+        v3.draw()
+        v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 180, length=length, width=width)
+        v4.draw()
+        v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 240, length=length, width=width)
+        v5.draw()
+        sd.line(start_point=v5.end_point, end_point=point, width=width)
+        print('It is hexagon')
+    if number_sides > 6:
+        print('Write number of sides from 3 to 6')
+    if number_sides < 3:
+        print('Write number of sides from 3 to 6')
+
+figure(point=point_5, number_sides=4, angle=30, length=100, width=2)
+
+
+
+
 
 sd.pause()
 
