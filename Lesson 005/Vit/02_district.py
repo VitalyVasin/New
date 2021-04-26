4c4c8578c7bee0b9a97a21287993da6f5c0293ef
vit <tomas_anderson@mail.ru>
2021-04-26 21:01:34 +0300

    Ready HomeWork 02 Lesson 005

--- Lesson 005/Vit/02_district.py
+++ Lesson 005/Vit/02_district.py
@@ -5,8 +5,22 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from district.central_street.house1 import room1 as csh1r1
+from district.central_street.house1 import room2 as csh1r2
+from district.central_street.house2 import room1 as csh2r1
+from district.central_street.house2 import room2 as csh2r2
+from district.soviet_street.house1 import room1 as ssh1r1
+from district.soviet_street.house1 import room2 as ssh1r2
+from district.soviet_street.house2 import room1 as ssh2r1
+from district.soviet_street.house2 import room2 as ssh2r2
 
 
+names = ", ".join(csh1r1.folks) + ', \n' + ", ".join(csh1r2.folks) + ', \n' + ", ".join(csh2r1.folks) + ', \n' + ", ".join(csh2r2.folks) \
+    + ', \n' + ", ".join(ssh1r1.folks) + ', \n' + ", ".join(ssh1r2.folks) + ', \n' + ", ".join(ssh2r1.folks) + ', \n' + ", ".join(ssh2r2.folks)
 
+print('На районе живут:', names)
+
+
+print('На районе живут:',  ", ".join(csh1r1.folks) + ', \n' + ", ".join(csh1r2.folks) + ', \n' + ", ".join(csh2r1.folks) + ', \n' + ", ".join(csh2r2.folks) \
+    + ', \n' + ", ".join(ssh1r1.folks) + ', \n' + ", ".join(ssh1r2.folks) + ', \n' + ", ".join(ssh2r1.folks) + ', \n' + ", ".join(ssh2r2.folks))
 
