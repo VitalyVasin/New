4a382300d0ea579c32a48f06a8c967d8fc807b60
vit <tomas_anderson@mail.ru>
2021-04-26 20:14:00 +0300

    Ready HomeWork 01 Lesson 005

--- Lesson 005/Vit/01_inhabitants.py
+++ Lesson 005/Vit/01_inhabitants.py
@@ -4,5 +4,12 @@
 # Формат: В комнате room_1 живут: ...
 
 # TODO здесь ваш код
+import sys
 
+import room_1 as r1
+import room_2 as r2
+name = []
 
+names_str = ",".join(r1.folks)
+names_str2 = ",".join(r2.folks)
+print('В комнате room_1 живут:', names_str, '\nВ комнате room_2 живут:', names_str2)
