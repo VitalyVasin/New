336371233bf677377390149a2647ec6487732cc5
vit <tomas_anderson@mail.ru>
2021-04-26 20:15:17 +0300

    Ready HomeWork 01 Lesson 005

--- Lesson 005/Vit/01_inhabitants.py
+++ Lesson 005/Vit/01_inhabitants.py
@@ -3,9 +3,6 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
-import sys
-
 import room_1 as r1
 import room_2 as r2
 name = []
