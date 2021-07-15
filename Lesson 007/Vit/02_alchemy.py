76bb5e6ff3e05fe58a2ad0ff3cf2176dfa7ae212
vit <tomas_anderson@mail.ru>
2021-07-15 15:48:11 +0300

    Ready HomeWork 02-1 Lesson 007

--- Lesson 007/Vit/02_alchemy.py
+++ Lesson 007/Vit/02_alchemy.py
@@ -19,7 +19,104 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+result_part = 1
+class Storm:
+    def __str__(self):
+        return 'Storm'
+
+class Water:
+
+    def __str__(self):
+        return 'Water'
+
+    def __add__(self, other):
+        return Result(part1=self, part2=other)
+
+
+class Air:
+    def __str__(self):
+        return 'Air'
+
+    def __add__(self, other):
+        return Result(part1=self, part2=other)
+
+class Fire:
+    def __str__(self):
+        return 'Fire'
+
+    def __add__(self, other):
+        return Result(part1=self, part2=other)
+
+class Soil:
+    def __str__(self):
+        return 'Soil'
+
+    def __add__(self, other):
+        return Result(part1=self, part2=other)
+
+
+class Result:
+    def __init__(self, part1, part2):
+        self.part1 = part1
+        self.part2 = part2
+        self.mix = 'Nothing'
+        if str(self.part1) == 'Water':
+            if str(self.part2) == 'Air':
+                self.mix = 'Storm'
+            elif str(self.part2) == 'Fire':
+                self.mix = 'Steam'
+            elif str(self.part2) == 'Soil':
+                self.mix = 'Dirt'
+            else:
+                self.mix = 'None'
+        elif str(self.part1) == 'Air':
+            if str(self.part2) == 'Water':
+                self.mix = 'Storm'
+            elif str(self.part2) == 'Fire':
+                self.mix = 'Lighting'
+            elif str(self.part2) == 'Soil':
+                self.mix = 'Dust'
+            else:
+                self.mix = 'None'
+        elif str(self.part1) == 'Fire':
+            if str(self.part2) == 'Water':
+                self.mix = 'Steam'
+            elif str(self.part2) == 'Air':
+                self.mix = 'Lighting'
+            elif str(self.part2) == 'Soil':
+                self.mix = 'Lava'
+            else:
+                self.mix = 'None'
+        elif str(self.part1) == 'Soil':
+            if str(self.part2) == 'Water':
+                self.mix = 'Dirt'
+            elif str(self.part2) == 'Air':
+                self.mix = 'Dust'
+            elif str(self.part2) == 'Fire':
+                self.mix = 'Lava'
+            else:
+                self.mix = 'None'
+
+        else:
+            self.mix = '= None'
+
+    def __eq__(self, other):
+        return self == other
+
+    def __str__(self):
+
+        return self.mix
+
+W = Water()
+A = Air()
+F = Fire()
+S = Soil()
+# result1 = F + S
+# print(result1)
+
+print(Water(), '+', Air(), '=', Water() + Air())
+print(Fire(), '+', Air(), '=', Fire() + Air())
+
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
