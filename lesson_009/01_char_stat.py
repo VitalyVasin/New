a8763950ccf9ab60cbccc2ea77fa4a623407c208
vit <tomas_anderson@mail.ru>
2021-11-12 15:32:58 +0300

    Ready HomeWork 01 Lesson 009 part 1

---
+++ lesson_009/01_char_stat.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+
+# Подсчитать статистику по буквам в романе Война и Мир.
+# Входные параметры: файл для сканирования
+# Статистику считать только для букв алфавита (см функцию .isalpha() для строк)
+#
+# Вывести на консоль упорядоченную статистику в виде
+# +---------+----------+
+# |  буква  | частота  |
+# +---------+----------+
+# |    А    |   77777  |
+# |    Б    |   55555  |
+# |   ...   |   .....  |
+# |    a    |   33333  |
+# |    б    |   11111  |
+# |   ...   |   .....  |
+# +---------+----------+
+# |  итого  | 9999999  |
+# +---------+----------+
+#
+# Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
+# Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+
+# TODO здесь ваш код
+import zipfile
+from pprint import pprint
+
+class Counter_char:
+    common_count = 0
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.stat = {}
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
+
+    def count(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+            print('Архив найден ')
+        else:
+            print('Архив не найден ')
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            print('Файл открыт '+self.file_name)
+            for line in file:
+                self._collect_for_line(line=line[:-1])
+            # print('Всего букв: ', self.common_count)
+
+    def _collect_for_line(self, line):
+        for char in line:
+            if char.isalpha():
+                if char in self.stat:
+                    self.stat[char] += 1
+                    self.common_count +=1
+                else:
+                    self.stat[char] = 1
+                    self.common_count +=1
+            # else:
+            #     None
+
+    # def _collect_for_line(self, line):
+    #     for char in line:
+    #         if char.isalpha():
+    #             if self.sequence in self.stat:
+    #                 if char in self.stat[self.sequence]:
+    #                     self.stat[self.sequence][char] += 1
+    #                 else:
+    #                     self.stat[self.sequence][char] = 1
+    #             else:
+    #                 self.stat[self.sequence] = {char: 1}
+    #             self.sequence = self.sequence[1:] + char
+    #         else:
+    #             None
+
+    def convert_stat(self):
+        print(f'+{"":-^9}+{"":-^10}+')
+        print(f'|{"буква": ^9}|{"частота": ^10}|')
+        print(f'+{"":-^9}+{"":-^10}+')
+        sorted_tuple = sorted(self.stat.items(), key=lambda x: x[1], reverse=True)
+        for key, value in sorted_tuple:
+            print(f'|{key: ^9}|{value: ^10}|')
+        print(f'+{"":-^9}+{"":-^10}+')
+        print(f'|{"итого": ^9}|{self.common_count: ^10}|')
+        print(f'+{"":-^9}+{"":-^10}+')
+
+
+counter = Counter_char(file_name='voyna-i-mir.txt.zip')
+counter.count()
+counter.convert_stat()
+# После выполнения первого этапа нужно сделать упорядочивание статистики
+#  - по частоте по возрастанию
+#  - по алфавиту по возрастанию
+#  - по алфавиту по убыванию
+# Для этого пригодится шаблон проектирование "Шаблонный метод" см https://goo.gl/Vz4828
