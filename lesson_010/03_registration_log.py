d255c2b04cb32480ee6bd9aa1a65baded9ef7149
vit <tomas_anderson@mail.ru>
2021-12-07 23:20:45 +0300

    Ready Homework 03 Lesson 010 add 2 logs for task (registration_bad.log and registrations_good.log)

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -23,3 +23,55 @@
 # Вызов метода обернуть в try-except.
 
 # TODO здесь ваш код
+class NotNameError(Exception):
+    None
+class NotEmailError(Exception):
+    None
+
+
+def registration(line):
+    name, email, age = line.split(' ')
+    if name and email and age:
+        if name.isalpha():
+            if "@" in email and "." in email:
+                if int(age) >= 10 and int(age) <= 99:
+                    # print('Основные проверки пройдены')
+                    # print('добавить запись в файл registrations_good.log')
+                    with open(r"registrations_good.log", "a") as file:
+                        file.write(line + '\n')
+                else:
+                    # print('добавить запись в файл registrations_bad.log')
+                    with open(r"registrations_bad.log", "a") as file:
+                        file.write(line + '\n')
+                    raise ValueError('Поле возраст НЕ является числом от 10 до 99')
+            else:
+                # print('добавить запись в файл registrations_bad.log')
+                with open(r"registrations_bad.log", "a") as file:
+                    file.write(line + '\n')
+                raise NotEmailError('Поле емейл НЕ содержит @ и .(точку)')
+        else:
+            # print('добавить запись в файл registrations_bad.log')
+            with open(r"registrations_bad.log", "a") as file:
+                file.write(line + '\n')
+            raise NotNameError('Поле имени содержит НЕ только буквы')
+    else:
+        # print('добавить запись в файл registrations_bad.log')
+        with open(r"registrations_bad.log", "a") as file:
+            file.write(line + '\n')
+        raise ValueError('НЕ присутсвуют все три поля')
+
+
+with open('registrations.txt', 'r') as ff:
+    for line in ff:
+        line = line[:-1]
+        try:
+            registration(line)
+        except ValueError as exc:
+            if 'unpack' in exc.args[0]:
+                print(f'НЕ присутсвуют все три поля, {exc} в строке {line}')
+            else:
+                print(f'{exc} в строке {line}')
+        except NotNameError as exc:
+            print(f'{exc} в строке {line}')
+        except NotEmailError as exc:
+            print(f'{exc} в строке {line}')
