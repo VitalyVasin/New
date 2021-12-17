df1863fda00d89bebdb9a03868927f39d5320280
vit <tomas_anderson@mail.ru>
2021-12-17 19:57:03 +0300

    Ready Homework 01 Lesson 011

--- lesson_011/Vit/01_shapes.py
+++ lesson_011/Vit/01_shapes.py
@@ -12,14 +12,20 @@ import simple_draw as sd
 #
 # Функция-фабрика должна принимать параметр n - количество сторон.
 
-
 def get_polygon(n):
-    pass
-    # TODO здесь ваш код
+    print(n)
+    def draw_triangle(point, angle, length):
+        point1 = point
+        for line_n in range(1, n): #заменить n на "n+1" и убрать строку 24 чтобы рисовалось векторами,
+                                   # но может быть нестыковка первого и последнего вектора
+            line = sd.get_vector(start_point=point1, angle=(angle + ((360 * line_n)/n)), length=length, width=1)
+            line.draw()
+            point1 = line.end_point
+        line = sd.line(start_point = point1, end_point = point, width=1)
 
+    return draw_triangle
 
-draw_triangle = get_polygon(n=3)
-draw_triangle(point=sd.get_point(200, 200), angle=13, length=100)
-
+draw_triangle = get_polygon(n=5)
+draw_triangle(point=sd.get_point(400, 200), angle=13, length=100)
 
 sd.pause()
